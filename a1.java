f8c2188d9d95658fd3c8c397b552716431bae9ad
tuguu <tuguldur8023@gmail.com>
2024-04-27 12:15:46 +0800

    l

--- a1.java
+++ a1.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class a1 {
     public static void main(String[] args) {
         System.out.println("Hi there sir.");
+        Scanner sc= new Scanner(System.in);
     }
 }
